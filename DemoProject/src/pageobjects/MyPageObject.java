0d58b3cd74b84ccda1afc623514b6088feaa44e4
Richard Clark <richard.clark@provartesting.com>
2018-10-26 09:46:18 +0100

    update pageobject for Amazon

--- DemoProject/src/pageobjects/MyPageObject.java
+++ DemoProject/src/pageobjects/MyPageObject.java
@@ -17,6 +17,23 @@ public class MyPageObject {
 	@TextType()
 	@FindBy(xpath = "//input[@name=\"q\"]")
 	public WebElement Search;
-	
+	@TextType()
+	@FindByLabel(label = "Search")
+	public WebElement search;
+	@LinkType()
+	@FindBy(linkText = "Salesforce.com For Dummies, 6th Edition (For Dummies (Computer/Tech))")
+	public WebElement salesforceComForDummies6ThEditionForDummiesComputerTech;
+	@ButtonType()
+	@FindByLabel(label = "Add to Cart")
+	public WebElement addToCart;
+	@LinkType()
+	@FindBy(linkText = "Proceed to checkout (1 item)")
+	public WebElement proceedToCheckout1Item;
+	@ButtonType()
+	@FindBy(id = "continue")
+	public WebElement ContinueBtn;
+	@TextType()
+	@FindBy(xpath = "//span[@class='price-large']/../../..//span[@id='newBuyBoxPrice']")
+	public WebElement buyNewOnClicking;
 			
 }
