c4a2616f11ed032980f4def412f6bfeffcf7bf31
Richard Clark <34280321+rclark-provar@users.noreply.github.com>
2018-11-20 16:53:13 +0000

    Update ad-debug.bat

--- ad-debug.bat
+++ ad-debug.bat
@@ -2,5 +2,7 @@ REM Debug environment variables before ANT task runs
 set PROVAR_SF_DemoAdmin=provardemo@provartesting.com
 set PROVAR_SF_DemoAdmin_password=Testing123
 
+google-chrome --version
+
 REM echo all settings
 set
