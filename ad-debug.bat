750feb788aab518e2d39a3aad5faf6aa4fd3b689
Richard Clark <34280321+rclark-provar@users.noreply.github.com>
2018-11-21 11:24:37 +0000

    Update ad-debug.bat

--- ad-debug.bat
+++ ad-debug.bat
@@ -3,6 +3,6 @@ set PROVAR_sf_DemoAdmin=provardemo@provartesting.com
 set PROVAR_sf_DemoAdmin_password=Testing123
 
 reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v DisplayVersion
-
+reg query "HKEY_CLASSES_ROOT\Wow6432Node\CLSID{5C65F4B0-3651-4514-B207-D10CB699B14B}\LocalServer32" /v DisplayVersion
 REM echo all settings
 set
