caa98f533480f7548d69b055979ef8b7acf98fd5
Richard Clark <34280321+rclark-provar@users.noreply.github.com>
2018-11-21 11:29:12 +0000

    Update ad-debug.bat

--- ad-debug.bat
+++ ad-debug.bat
@@ -2,7 +2,6 @@ REM Debug environment variables before ANT task runs
 set PROVAR_sf_DemoAdmin=provardemo@provartesting.com
 set PROVAR_sf_DemoAdmin_password=Testing123
 
-reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v DisplayVersion
-reg query "HKEY_CLASSES_ROOT\Wow6432Node\CLSID{5C65F4B0-3651-4514-B207-D10CB699B14B}\LocalServer32" /v DisplayVersion
+dir c:/Users/VssAdministrator/AppData/Local/Google/Chrome/Application
 REM echo all settings
 set
